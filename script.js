5f74cab018975651d315a227b467094173fee331
laura devere <lauradevh@gmail.com>
2016-02-22 21:36:42 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,74 @@
+function overlay(){
+	el = document.getElementById("overlay");
+	el.style.visibility = (el.style.visibility === "visible") ? "hidden" : "visible";
+}
+
+// TOOGLE VIEWS
+$(document).ready(function() {
+  $('#gifting').click(function() {
+    $(this).slideToggle();
+  });
+});
+
+var divs = ["toggle-reservation-details", "gifting", "card-payment"];
+var visibleDivId = null;
+function toggleVisibility(divId) {
+	if(visibleDivId === divId) {
+		} else {
+			visibleDivId = divId;
+		}
+			hideNonVisibleDivs();
+}	
+// toggleVisibility('gifting');
+function hideNonVisibleDivs() {
+	var i, divId, div;
+		for(i = 0; i < divs.length; i++) {
+			divId = divs[i];
+			div = document.getElementById(divId);
+		if(visibleDivId === divId) {
+			div.style.display = "block";
+		} else {
+			div.style.display = "none";
+		}
+	}
+}
+
+// TOGGLE DOT color
+
+var dots = ["first-dot", "sec-dot", "third-dot"];
+var visibleDotId = null;
+function toggleDotColor(dotId) {
+	if(visibleDotId === dotId) {
+		} else {
+			visibleDotId = dotId;
+		}
+			hideNonVisibleDots();
+}	
+
+function hideNonVisibleDots() {
+	var i, dotId, dot;
+		for(i = 0; i < dots.length; i++) {
+			dotId = dots[i];
+			dot = document.getElementById(dotId);
+		if(visibleDotId === dotId) {
+			dot.style.backgroundColor = "#e68443";
+		} else {
+			dot.style.backgroundColor = "#b7b7b7";
+		}
+	}
+}
+
+// Change div header
+function changeHeader(id, text){
+	document.getElementById(id).innerHTML = text;
+}
+
+// GIFTS
+function selectGift(id){
+	el = document.getElementById(id);
+	if(el.className !== "toggled") {
+		el.className = "toggled"
+	} else{
+		el.className = '';
+	}
+}
